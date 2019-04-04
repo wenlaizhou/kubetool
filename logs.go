f9dce48df819880267c3c4358b0ae253b5596bbb
zhouwl <zhouwl@missfress.cn>
2019-04-04 18:40:08 +0800

    日志长度降为200条

--- logs.go
+++ logs.go
@@ -2,7 +2,7 @@ package kubetool
 
 // 获取pod日志
 func GetLog(cluster KubeCluster, pod string, ns string) (string, error) {
-	return ExecKubectl(cluster, CmdLogs, "--all-containers=true", pod, "-n", ns, "--tail=400")
+	return ExecKubectl(cluster, CmdLogs, "--all-containers=true", pod, "-n", ns, "--tail=200")
 }
 
 // 获取pod所有日志

a744f94cb44538db50fbd90bd8320cae6e74274b
zhouwl <zhouwl@missfress.cn>
2019-06-25 11:34:48 +0800

    新增配置扫描

--- apply.go
+++ apply.go
@@ -2,6 +2,7 @@ package kubetool
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -22,17 +23,9 @@ func init() {
 	middleware.Mkdir(DeployYamlPath)
 }
 
-// 替换服务
-func Replace(content string) (string, error) {
-	timeStr := time.Now().Format("2006_1_2_15_04_05")
-	middleware.Mkdir("deploy")
-	_, _ = middleware.WriteString(fmt.Sprintf("deploy/%s.yaml", timeStr), content)
-	return "", nil
-}
-
 // 部署服务
 func Apply(cluster KubeCluster, content string, update bool) (string, error) {
-	fileName := fmt.Sprintf("%s/%s/%s.yaml", CurrentDir, DeployYamlPath, time.Now().Format(TimeStr))
+	fileName := fmt.Sprintf("%s/%s/%s_%d.yaml", CurrentDir, DeployYamlPath, time.Now().Format(TimeStr), rand.Int()%100)
 	_, _ = middleware.WriteString(fileName, content)
 	if update {
 		return KubeApi(cluster, CmdApply, "-f", fileName, "--cascade=true") // , "--prune=true", "--all")

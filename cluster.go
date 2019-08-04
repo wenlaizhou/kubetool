db350df9756e8c3a9d5aa63c135bd6442bf3f41a
zhouwl <zhouwl@missfress.cn>
2019-08-04 22:26:03 +0800

    代码格式化

--- cluster.go
+++ cluster.go
@@ -36,6 +36,28 @@ func Init(confPath string) {
 	_ = filepath.Walk(confPath, walkPath)
 }
 
+// 加入集群
+// 配置 或者 配置文件路径
+// 集群名称
+func JoinCluster(confPath string) error {
+	clusterName := strings.Replace(confPath, ".config", "", -1)
+	// 重复集群
+	cachePath := fmt.Sprintf("%s/conf/%s/cache", currentDir, clusterName)
+	// hasCluster, _ := Cluster[clusterName]
+	conf := middleware.ReadString(confPath)
+	if len(conf) <= 0 {
+
+	}
+	Cluster[clusterName] = KubeCluster{
+		Name:      clusterName,
+		ConfPath:  confPath,
+		CachePath: cachePath,
+		Conf:      conf,
+	}
+	middleware.Mkdir(cachePath)
+	return nil
+}
+
 // 漫游配置路径
 func walkPath(path string, info os.FileInfo, err error) error {
 	if !strings.HasSuffix(info.Name(), ".config") {

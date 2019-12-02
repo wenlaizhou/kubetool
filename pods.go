6f10980f1308231336e99850a4ec47f9f0742550
zhouwl <zhouwl@missfress.cn>
2019-12-02 18:50:57 +0800

    新增node获取pod列表接口

--- pods.go
+++ pods.go
@@ -184,7 +184,7 @@ func GetPodsByNode(cluster KubeCluster, node string) (kubetype.PodList, error) {
 		return result, errors.New("node 为空")
 	}
 	args := []string{CmdGet, "po", ArgsAllNamespaces, "-o", "json"}
-	args = append(args, fmt.Sprintf("--field-selector='spec.nodeName=%s'", node))
+	args = append(args, fmt.Sprintf("--field-selector=spec.nodeName=%s", node))
 	res, err := KubeApi(cluster, args...)
 	if err != nil {
 		return result, err
